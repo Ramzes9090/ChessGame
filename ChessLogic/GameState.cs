92b83799d619a33f6d3370abaefcdf0be8e86063
Daniel Śliwka <danielsliwkado@gmail.com>
2023-12-28 14:22:12 +0100

    added check mate handeling

--- ChessLogic/GameState.cs
+++ ChessLogic/GameState.cs
@@ -10,6 +10,7 @@ namespace ChessLogic
     {
         public Board Board { get; }
         public Player CurrentPlayer { get; private set; }
+        public Result Result { get; private set; } = null;
         public GameState(Player player, Board board)
         {
             CurrentPlayer = player;
@@ -24,13 +25,36 @@ namespace ChessLogic
             }
 
             Piece piece = Board[pos];
-            return piece.GetMoves(pos, Board);
+            IEnumerable<Move> moveCandidates =  piece.GetMoves(pos, Board);
+            return moveCandidates.Where(move => move.IsLegal(Board));
         }
         public void MakeMove(Move move)
         {
             move.Execute(Board);
             CurrentPlayer = CurrentPlayer.Opponent();
         }
-
+        public IEnumerable<Move> AllLegalMovesFor(Player player)
+        {
+            IEnumerable<Move> moveCandidates = Board.PiecePositionsFor(player).SelectMany(pos=>
+            {
+                Piece piece = Board[pos];
+                return piece.GetMoves(pos, Board);
+            });
+            return moveCandidates.Where(move=>move.IsLegal(Board));
+        }
+        public void CheckForGameOver()
+        {
+            if (!AllLegalMovesFor(CurrentPlayer).Any())
+            {
+                if (Board.IsInCheck(CurrentPlayer))
+                {
+                    Result = Result.Win(CurrentPlayer.Opponent());
+                }
+                else
+                {
+                    Result = Result.Draw(EndReason.Stalemate);
+                }
+            }
+        }
     }
 }
