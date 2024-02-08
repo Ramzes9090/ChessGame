838067969f34a46f4e4b2a20b21351951f5da8af
Daniel Śliwka <danielsliwkado@gmail.com>
2024-02-08 09:36:23 +0100

    added enPassant

--- ChessLogic/Pieces/Pawn.cs
+++ ChessLogic/Pieces/Pawn.cs
@@ -1,9 +1,4 @@
 ﻿using ChessLogic.Moves;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ChessLogic
 {
@@ -76,7 +71,7 @@ namespace ChessLogic
                 Position twoMovePos = oneMovePosition + forward;
                 if(!HasMoved && CanMoveTo(twoMovePos, board))
                 {
-                    yield return new NormalMove(from, twoMovePos);
+                    yield return new DoublePawn(from, twoMovePos);
                 }
             }
         }
@@ -85,7 +80,11 @@ namespace ChessLogic
             foreach (Direction dir in new Direction[] {Direction.West, Direction.East })
             {
                 Position to = from + forward + dir;
-                if (CanCaptureAt(to, board))
+                if (to == board.GetPawnSkipPosition(Color.Opponent()))
+                {
+                    yield return new EnPassant(from, to);
+                }
+                else if (CanCaptureAt(to, board))
                 {
                     if (to.Row == 0 || to.Row == 7)
                     {
